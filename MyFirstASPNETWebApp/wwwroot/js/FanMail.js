28a30b78c7c863eb6a453087d685cb09f3fd16bf
kayleeols <kayleeols@yahoo.com>
2022-01-12 18:48:11 -0700

    Add project files.

---
+++ MyFirstASPNETWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,7 @@
+﻿$("#btnSend").click(function () {
+    alert("From: " + $("#txtFrom").val() +
+        ", Subject: " + $("#txtSubject").val() +
+        ", Message: " + $("#txtMessage").val());
+
+    $("#picGanderson").fadeToggle(); //Will make image fade in and out when the submit button is hit
+})
